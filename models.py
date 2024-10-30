1d9cd20f9da8ec09aa06a917ef7f7de82717534e
nadineelnaggar <nadine_el_naggar@hotmail.com>
2024-10-30 15:53:46 +0400

    iterate through the input tensor and feed it to DNNC token by token

--- models.py
+++ models.py
@@ -504,6 +504,7 @@ class RecurrentDNNC(nn.Module):
         # for i in range(length.item()):
         for i in range(x.size()[0]):
             x[0][i] = self.dnnc(x[0][i])
+            print('x[0][',i,'] = ',x[0][i])
         print('x after DNNC ',x)
 
         # x, _ = pad_packed_sequence(x, batch_first=True)
